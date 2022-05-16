003428e2a4c5e67905b98bccf25335a35a4174e9
tinkaracadez <tinkara.cadez@gmail.com>
2022-05-16 08:41:10 +0200

    omogočila iskanje po več spremenljivkah

--- static/lokalna_veriga.js
+++ static/lokalna_veriga.js
@@ -1,14 +1,18 @@
 'use strict'
 
-function isci() {
+function isci(stolpci = [1]) {
     let vrednost = document.getElementById('isci').value;
     let tabela = document.getElementById('izpis');
     [...tabela.rows].forEach(vrstica => {
-        let vsebina = vrstica.cells[1].innerText
-        if(vsebina.toLocaleLowerCase().indexOf(vrednost.toLocaleLowerCase()) >= 0) {
+        if(stolpci.some(stolpec => seUjema(vrstica, stolpec, vrednost))) { // some = vsaj eden mora biti true
             vrstica.style.display = ''
         } else {
             vrstica.style.display = 'none'
         }
     })
+}
+
+function seUjema(vrstica, stolpec, vrednost) { // ali se vrstica v stulpcu 'stolpec' ujema z vrednostjo
+    let vsebina = vrstica.cells[stolpec].innerText
+    return vsebina.toLocaleLowerCase().indexOf(vrednost.toLocaleLowerCase()) >= 0
 }
